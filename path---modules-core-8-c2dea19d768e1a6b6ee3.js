f413454a342b6f391b3462dd931f6c99d0128804
Lev Rechnik <relev@pisem.net>
2021-08-27 22:01:15 +0300

    Updates

---
+++ path---modules-core-8-c2dea19d768e1a6b6ee3.js
@@ -0,0 +1,2 @@
+webpackJsonp([41472708559843],{395:function(t,l){t.exports={data:{markdownRemark:{html:"<p>Модуль позволяет разместить на странице системы публичную часть выбранных к публикации реестров в машиночитаемой форме.</p>",frontmatter:{date:"July 01, 2021",dir_url:"/modules",dir_url_title:"Все модули",icon:"/images/usluga.svg",title:"Модуль публикации цифровых реестров",itemList:null,captionTitle:null,captionText:null,lastItemCaption:null,lastItemText:null}}},pathContext:{}}}});
+//# sourceMappingURL=path---modules-core-8-c2dea19d768e1a6b6ee3.js.map
